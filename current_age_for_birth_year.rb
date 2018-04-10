024bd8a9df07dafeae62a03b5611a47ec7ed18bd
Julian Johannesen <julian.johannesen@gmail.com>
2018-04-10 20:14:39 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,11 @@
+def current_age_for_birth_year(birth_year)
+  age = Time.now.year - birth_year
+  puts "You are #{age} years old."
+end
+
 puts "What year were you born?"
 birth_year = gets.to_i
 if birth_year
   current_age_for_birth_year
 end
-def current_age_for_birth_year(birth_year)
-  age = Time.now.year - birth_year
-  puts "You are #{age} years old."
-end
+
